4f18af2f2119572360e8de0469a3d0f8bc14503c
Vitalick <miraigeweb@gmail.com>
2022-11-23 12:28:54 +0300

    made a switch and activation of navigation

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -30,7 +30,8 @@ function Header() {
                     </div>
                 </div>}
             <div className={st.itemImg}>
-                <img onClick={clickImg}
+                <img className={st.imgRotate}
+                     onClick={clickImg}
                      src="https://media-exp1.licdn.com/dms/image/C4D0BAQGqcuVe6n8FXw/company-logo_100_100/0/1655710554698?e=2147483647&v=beta&t=HrutFC8kYr_MJayfs8r52nMz0zBLLn_XTRdtwQxbhN0"
                      alt="arrow"
                 />
