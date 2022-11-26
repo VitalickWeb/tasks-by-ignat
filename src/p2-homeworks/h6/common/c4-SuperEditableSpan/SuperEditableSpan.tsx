b1dd7c08db86195990a96c484771e44335ca4ac9
Vitalick <miraigeweb@gmail.com>
2022-11-26 17:21:19 +0300

    HW6 fixed localStorage

--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -67,7 +67,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
-                        {children || restProps.value}
+                        &#9998;{children || restProps.value}
                     </span>
                 )
             }
